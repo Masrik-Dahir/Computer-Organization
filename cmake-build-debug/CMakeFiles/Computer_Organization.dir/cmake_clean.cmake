d58dcfe56b6aa9f2bf2d6c934554f83fce28fde7
Masrik <dahirma@vcu.edu>
2021-10-15 22:47:33 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Computer_Organization.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Computer_Organization.dir/main.c.obj"
+  "Computer_Organization.exe"
+  "Computer_Organization.exe.manifest"
+  "Computer_Organization.pdb"
+  "libComputer_Organization.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Computer_Organization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
